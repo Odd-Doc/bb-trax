108f0e7b301b88200b1ccbe5346692f6699edd41
Odd-Doc <gsphillipsjr@gmail.com>
2024-09-26 10:27:00 -0500

    TODO: add grid layout for device list on deviceListScreen

--- components/deviceList.jsx
+++ components/deviceList.jsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import colorPalette from "../styles/color-palette";
 const DeviceList = ({ devices }) => {
   return (
     <View style={styles.devicesContainer}>
       <View style={styles.deviceListHeaderContainer}>
-        <Text>Hazard ID</Text>
-        <Text>Serial Number</Text>
-        <Text>Model</Text>
+        <Text style={styles.deviceListHeaderText}>Hazard ID</Text>
+        <Text style={styles.deviceListHeaderText}>Serial Number</Text>
+        <Text style={styles.deviceListHeaderText}>Model</Text>
       </View>
       {devices.length > 0 && (
         <FlatList
@@ -38,19 +38,24 @@ const DeviceList = ({ devices }) => {
 export default DeviceList;
 
 const styles = StyleSheet.create({
+  devicesContainer: {
+    backgroundColor: colorPalette.aliceblue,
+    flex: 1,
+  },
   deviceTextContainer: {
     backgroundColor: "#97a770",
     marginLeft: 10,
     flex: 1,
     alignItems: "center",
   },
-  devicesContainer: {
-    backgroundColor: "#555555",
-  },
   deviceListHeaderContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
     position: "static",
+  },
+  deviceListHeaderText: {
+    fontFamily: "Roboto_400Regular",
+    fontSize: 16,
   },
   device: {
     backgroundColor: "#ba6464",
