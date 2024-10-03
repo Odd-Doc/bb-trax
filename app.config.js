a52d31137b37ab4f49146ebb5adae3a25c9b4b86
Odd-Doc <gsphillipsjr@gmail.com>
2024-10-03 09:48:32 -0500

    added app.config to enable use of .env api keys.

---
+++ app.config.js
@@ -0,0 +1,23 @@
+import { ExpoConfig, ConfigContext } from "@expo/config";
+import * as dotenv from "dotenv";
+
+// initialize dotenv
+dotenv.config();
+
+export default (config) => ({
+  ...config,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.odddoc.bbtrax",
+    config: {
+      googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLEMAPS_APIKEY,
+    },
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    package: "com.odddoc.bbtrax",
+  },
+});
